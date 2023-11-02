19cebb5da16edd8f45ebcf88579813c5b578f348
MayraLeticia <mayraleticia03@gmail.com>
2023-11-02 20:11:49 -0300

    alteracoes feitas na sala dia 1/11

---
+++ frontend/src/components/Button/index.jsx
@@ -0,0 +1,9 @@
+import './style.scss'
+
+const Button = (name) => {
+    return(
+        <button className='button-primary'>{name}</button>
+    )
+}
+
+export default Button;
--- frontend/src/screens/Login/index.jsx
+++ frontend/src/screens/Login/index.jsx
@@ -1,8 +1,19 @@
-import { Column } from "../../components";
+import { Button, Column, Row, TextField } from "../../components";
 
 const Login = () => {
     return(
-        <Column>login</Column>
+        <Row>
+            <Column>
+                <TextField name='Matrícula' />
+                <TextField name='Senha' />
+                <Row>
+                    <a>Já possui cadastro?</a>
+                    <a>Esqueci minha senha</a>
+                </Row>
+                <Button>Entrar na plataforma</Button>
+            </Column>
+            <Column></Column>
+        </Row>
     )
 }
 
